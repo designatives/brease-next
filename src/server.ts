5ebdcfbc690e8c040c706a0f7615a413f79377ac
Norbert Berenyi <norbert@berenyi.dev>
2025-10-20 10:50:10 +0200

    fix: formatting

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,3 @@
-export type {
-  BreaseRedirect,
-} from './types.js';
+export type { BreaseRedirect } from './types.js';
 
-export {
-  fetchRedirects,
-} from './api.js';
+export { fetchRedirects } from './api.js';
